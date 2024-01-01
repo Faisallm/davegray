c6a3d4d47786743d1ff0311c2653b0fe93e37eaa
Faisallm <faisallawan1997@gmail.com>
2024-01-01 13:37:00 -0800

    Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 // we can import image
-import logo from './logo.svg';
+import logo from "./logo.svg";
 // we can import css
-import './App.css';
+import "./App.css";
 
 function App() {
-  const name =  "Faisal";
+  const handleNameChange = () => {
+    const names = ["Faisal", "Lawan", "Muhammad"]
+    // randomly generate a number between 0 and 2
+    const int = Math.floor(Math.random()*3)
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see {name}.
+          Hello {handleNameChange()}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
